5b906ba6b4579be44aebc86d602c0259894e5936
Sean B. <sdbazoes@gmail.com>
2023-10-28 23:01:59 -0400

    preliminary constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,26 +4,59 @@
 
 package frc.robot;
 
-import static frc.lib.swerve.SwerveDescription.*;
+import edu.wpi.first.math.util.Units;
+import frc.lib.swerve.SwerveDescription.CANIDs;
+import frc.lib.swerve.SwerveDescription.Dimensions;
+import frc.lib.swerve.SwerveDescription.EncoderOffsets;
+import frc.lib.swerve.SwerveDescription.Gearing;
+import frc.lib.swerve.SwerveDescription.Inversion;
+import frc.lib.swerve.SwerveDescription.Physics;
+import frc.lib.swerve.SwerveDescription.PidGains;
 
 public final class Constants {
   public static final class Swerve {
-    public static final Dimensions dimensions = new Dimensions(0,0);
+    public static final Dimensions dimensions = new Dimensions(Units.inchesToMeters(24.75), Units.inchesToMeters(24.75));
 
-    public static final CANIDs frontLeftIDs = new CANIDs(0, 0, 0);
-    public static final CANIDs frontRighIDs = new CANIDs(0, 0, 0);
-    public static final CANIDs rearLeftIDs = new CANIDs(0, 0, 0);
-    public static final CANIDs rearRightIDs = new CANIDs(0, 0, 0);
+    public static final CANIDs frontLeftIDs = new CANIDs(8, 10, 9);
+    public static final CANIDs frontRighIDs = new CANIDs(5, 7, 6);
+    public static final CANIDs rearLeftIDs = new CANIDs(11, 13, 12);
+    public static final CANIDs rearRightIDs = new CANIDs(2, 4, 3);
 
-    public static final Gearing gearing = new Gearing(0, 0, 0, 0);
-    public static final EncoderOffsets offsets = new EncoderOffsets(0, 0, 0, 0);
-    public static final Inversion inversion = new Inversion(false, true);
-    public static final Physics physics = new Physics(0, 0, 0, 0);
+    public static final Gearing gearing = new Gearing(DriveGearRatios.SDSMK4i_L2, ((150.0 / 7.0) / 1.0), 2.0, 0);
+    public static final EncoderOffsets offsets = new EncoderOffsets(267.09, 350.15, 348.83, 57.39); //degrees
+    public static final Inversion inversion = new Inversion(false, false); //todo
+    public static final Physics physics = new Physics(0.001, 0.0001, 800, 10); //todo
   
-    public static final PidGains driveGains = new PidGains(0, 0, 0, 0, 0);
-    public static final PidGains angleGains = new PidGains(0, 0, 0, 0, 0);
+    public static final PidGains driveGains = new PidGains(0.05, 0, 0, 0, 0); //todo
+    public static final PidGains angleGains = new PidGains(0.15, 0, 0, 0, 0); //todo
   
-    public static final int pigeonCANId = 0;
-    public static final boolean invertSteerMotors = false;
+    public static final int pigeonCANId = 14;
+    public static final boolean invertSteerMotors = true;
   }
+
+  public class DriveGearRatios{
+    /* SDS MK3 */
+    /** SDS MK3 - 8.16 : 1 */
+    public static final double SDSMK3_Standard = (8.16 / 1.0);
+    /** SDS MK3 - 6.86 : 1 */
+    public static final double SDSMK3_Fast = (6.86 / 1.0);
+
+    /* SDS MK4 */
+    /** SDS MK4 - 8.14 : 1 */
+    public static final double SDSMK4_L1 = (8.14 / 1.0);
+    /** SDS MK4 - 6.75 : 1 */
+    public static final double SDSMK4_L2 = (6.75 / 1.0);
+    /** SDS MK4 - 6.12 : 1 */
+    public static final double SDSMK4_L3 = (6.12 / 1.0);
+    /** SDS MK4 - 5.14 : 1 */
+    public static final double SDSMK4_L4 = (5.14 / 1.0);
+    
+    /* SDS MK4i */
+    /** SDS MK4i - 8.14 : 1 */
+    public static final double SDSMK4i_L1 = (8.14 / 1.0);
+    /** SDS MK4i - 6.75 : 1 */
+    public static final double SDSMK4i_L2 = (6.75 / 1.0);
+    /** SDS MK4i - 6.12 : 1 */
+    public static final double SDSMK4i_L3 = (6.12 / 1.0);
+}
 }
