f3eb39f92fc4b93350093ad863fc8cdf13dfc07f
Sean B. <sdbazoes@gmail.com>
2023-12-22 18:14:52 -0500

    more realistic sim constants

--- src/main/java/frc/lib/telemetry/BuildConstants.java
+++ src/main/java/frc/lib/telemetry/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "base-swerve-2024";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 140;
-    public static final String GIT_SHA = "bc3315e68ed976afafa077a856416ade3a19d100";
-    public static final String GIT_DATE = "2023-12-22 16:36:12 EST";
+    public static final int GIT_REVISION = 141;
+    public static final String GIT_SHA = "1dd7a1ff2b2c304bf4e95042a307cc37bba692f5";
+    public static final String GIT_DATE = "2023-12-22 17:58:37 EST";
     public static final String GIT_BRANCH = "needs-testing";
-    public static final String BUILD_DATE = "2023-12-22 17:57:32 EST";
-    public static final long BUILD_UNIX_TIME = 1703285852577L;
+    public static final String BUILD_DATE = "2023-12-22 18:07:29 EST";
+    public static final long BUILD_UNIX_TIME = 1703286449221L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -68,7 +68,7 @@ public final class Constants {
     public static final Inversion inversion = new Inversion(false, true, true, false); //herra 4.5, 6
 
     //inertia only used for simulation
-    public static final Physics physics = new Physics(0.001,0.01, Robot.isReal() ? 50 : 800, 10);
+    public static final Physics physics = new Physics(0.05,0.01, Robot.isReal() ? 50 : 800, 10);
     public static final double steerMotorCurrentLimit = Robot.isReal() ? 40 : 120; //amps
     
     public static final PidGains driveGains = new PidGains(0.35, 0, 0, 0.11, 0.3); 
