203cff67bf03a6a16421a56218a9f90d69243bd1
Sean B. <sdbazoes@gmail.com>
2023-12-29 19:18:31 -0500

    whatever

--- src/main/java/frc/lib/telemetry/BuildConstants.java
+++ src/main/java/frc/lib/telemetry/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "base-swerve-2024";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 153;
-    public static final String GIT_SHA = "7b8b0e648757277c7be8699dac68ffcee0788eb0";
-    public static final String GIT_DATE = "2023-12-26 22:12:45 EST";
+    public static final int GIT_REVISION = 154;
+    public static final String GIT_SHA = "c8589566fe2d5a782d0e791eb1e2a94496b89cfd";
+    public static final String GIT_DATE = "2023-12-29 16:19:32 EST";
     public static final String GIT_BRANCH = "needs-testing";
-    public static final String BUILD_DATE = "2023-12-29 10:34:18 EST";
-    public static final long BUILD_UNIX_TIME = 1703864058479L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2023-12-29 19:15:06 EST";
+    public static final long BUILD_UNIX_TIME = 1703895306470L;
+    public static final int DIRTY = 0;
 
     private BuildConstants(){}
 }
