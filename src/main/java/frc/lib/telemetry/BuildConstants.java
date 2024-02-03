a322fcbea0468081b843910d3b0ad1cc0ba03483
frc-3461 <programming@peacce.org>
2024-02-03 14:07:15 -0500

    stupidity

--- src/main/java/frc/lib/telemetry/BuildConstants.java
+++ src/main/java/frc/lib/telemetry/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "frc-2024";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = -1;
-    public static final String GIT_SHA = "UNKNOWN";
-    public static final String GIT_DATE = "UNKNOWN";
-    public static final String GIT_BRANCH = "UNKNOWN";
-    public static final String BUILD_DATE = "2024-01-31 19:01:22 EST";
-    public static final long BUILD_UNIX_TIME = 1706745682562L;
-    public static final int DIRTY = 129;
+    public static final int GIT_REVISION = 193;
+    public static final String GIT_SHA = "3be48c1fd21296b71fb358f7f8521f2a1f8567ef";
+    public static final String GIT_DATE = "2024-02-01 18:08:53 EST";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2024-02-01 18:18:37 EST";
+    public static final long BUILD_UNIX_TIME = 1706829517203L;
+    public static final int DIRTY = 0;
 
     private BuildConstants(){}
 }
