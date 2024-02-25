c2217f0c2b7c72e3dbd1a275b2b4ae9d9aa7ecb7
Sean B. <sdbazoes@gmail.com>
2024-02-24 22:35:22 -0500

    who renamed statemachine

--- src/main/java/frc/lib/telemetry/BuildConstants.java
+++ src/main/java/frc/lib/telemetry/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "frc-2024";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 215;
-    public static final String GIT_SHA = "dad2b8d47debc2f836bc3145784910a6ddcf44b1";
-    public static final String GIT_DATE = "2024-02-23 21:54:22 EST";
+    public static final int GIT_REVISION = 217;
+    public static final String GIT_SHA = "296e51870bf3a8358f63a2d678c7a6f25b39279f";
+    public static final String GIT_DATE = "2024-02-24 16:55:58 EST";
     public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2024-02-24 14:42:32 EST";
-    public static final long BUILD_UNIX_TIME = 1708803752402L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2024-02-24 22:00:13 EST";
+    public static final long BUILD_UNIX_TIME = 1708830013166L;
+    public static final int DIRTY = 0;
 
     private BuildConstants(){}
 }
